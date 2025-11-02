7ed21d79cbb24cdbb5337b97724a519fdda127d0
devil <europedreadlydevil@gmail.com>
2025-11-02 19:43:49 +0300

    bug(#13): Add library documentation to crate root
    This commit adds library-level documentation to the crate root,
    providing a basic usage example and a link to the official
    Firecracker documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,14 @@
+//! A ***Non-official*** library for easy work with FirecrackerVM from Rust.
+//!
+//! Exemple:
+//! ```no_run
+//! let vm_process = FirecrackerStartup::new()
+//!     .api_sock("/tmp/some.socket")
+//!     .start().unwrap();
+//!
+//! let vm_socket = VMSocket::new().unwrap();
+//! let vm = vm_socket.connect("/tmp/some.socket");
+//! ```
+//! Before starting work, we recommend that you familiarize yourself with the official [documentation](https://github.com/firecracker-microvm/firecracker/blob/main/docs/getting-started.md).
+
 pub mod vm;
