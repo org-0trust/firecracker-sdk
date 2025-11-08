f66412bd0cc54d08a2410ea36d071c1134691361
devil <europedreadlydevil@gmail.com>
2025-11-08 18:34:30 +0300

    bug(#50): Print stdout in startup_w_stdout test
    This change adds a print statement to the startup_w_stdout test to
    print the stdout of the firecracker process. This is useful for
    debugging purposes

--- tests/firecracker_startup/startup_w_stdout.rs
+++ tests/firecracker_startup/startup_w_stdout.rs
@@ -4,6 +4,8 @@ use firecracker_sdk::api::startup::FirecrackerStartup;
 #[tokio::test]
 async fn startup_w_stdout() -> Result<()> {
     let mut process = FirecrackerStartup::new().stdout(true).start().await?;
-    assert!(!process.stdout().await?.is_empty());
+    let out = process.stdout().await?;
+    assert!(!out.is_empty());
+    println!("{out}");
     Ok(())
 }
